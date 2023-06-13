5eb68e2499baae08ffebdf83a5e1ab078a659af3
ALDO ALBERTO ARBIZU <69614658+JinseiKaizen@users.noreply.github.com>
2023-06-12 23:47:01 -0300

    Update productManager.js

--- productManager.js
+++ productManager.js
@@ -1,192 +1,124 @@
-const PRODUCTS = "products.txt";
+const fs = require('fs')
 
-const fs = require("fs");
+const FILE = 'products.json'
 
-// Clase ProductManager para administrar productos
 class ProductManager {
+  static id = 0
+
   constructor() {
-    this.products = [];
-    this.path = PRODUCTS;
-    this.automaticId = 1;
-  }
-
-   // Agrega un producto a la lista
-  async addProduct(title, description, price, thumbnail, code, stock) {
+    this.props = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
+    this.path = FILE;
     try {
-      // Comprueba si el código ya existe
-      const existCode = this.products.find((prod) => prod.code === code);
-      if (existCode) {
-        console.log(
-          `El código ${code} coincide con un código ya existente de ${existCode.title} dentro de productos`
-        );
-      }
-
-      // Comprueba si el ID ya está en uso
-      const existId = this.products.find(
-        (prod) => prod.id === this.automaticId
-      );
-      if (existId) {
-        console.log(
-          `El ID ${this.automaticId} ya está en uso, no se puede agregar el producto`
-        );
-      }
-
-      // Comprueba si todos los campos están completos
-      if (!title || !description || !price || !thumbnail || !code || !stock) {
-        console.log("Por favor complete todos los campos solicitados");
-      } else {
-
-        const product = {
-          id: this.automaticId++,
-          title,
-          description,
-          price,
-          thumbnail,
-          code,
-          stock,
-        };
-
-        // Agrega el producto a la lista
-        this.products.push(product);
-        console.log(`El producto ${title} fue agregado correctamente`);
-
-        let text = JSON.stringify(this.products, null, 2);
-
-        fs.writeFileSync(PRODUCTS, text, (error) => console.log(error));
-      }
+      this.products = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
     } catch (error) {
-      console.log(error);
+      this.products = []
     }
+    ProductManager.id = this.products.reduce((prev, curr) => (
+      curr.id >= prev ? curr.id : prev
+    ), 0)
   }
 
-  // Obtiene la lista de productos
   async getProducts() {
     try {
-      if (this.products.length === 0) {
-        console.log("No hay productos disponibles");
-      } else {
-        console.log("Productos disponibles:");
-        this.products.forEach((product) => {
-          console.log(product);
-        });
-      }
+      return this.products;
     } catch (error) {
       console.log(error);
     }
   }
 
-  // Obtiene un producto por su ID
+  isValidateCode(product) {
+    return this.products.some(item => item.code === product.code);
+  }
+
+  async addProduct(product) {
+    try {
+      for (let prop of this.props) {
+        if (!product.hasOwnProperty(prop) || this.isValidateCode(product)) {
+          return 'Producto invalido!';
+        }
+      }
+
+      this.products = [...this.products, { id: ++ProductManager.id, ...product }];
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2))
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async getProductById(id) {
     try {
-      const productExistent = this.products.find((prod) => prod.id === id);
-      if (!productExistent) {
-        console.log(`Not found: el producto con el id ${id} no fue encontrado`);
-      } else {
-        console.log(`El producto con el ID ${id} fue encontrado`);
-        console.log(productExistent);
+      let productId = this.products.find(product => product.id === id);
+      return productId ?? 'Not found';
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updateProduct(index, field, newValue) {
+    try {
+      const updateProductIndex = this.products.findIndex(product => product.id === index)
+      if (updateProductIndex !== -1) {
+        this.products[updateProductIndex][field] = newValue;
+        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
       }
     } catch (error) {
       console.log(error);
     }
   }
 
-  // Actualiza un campo de un producto
-  async updateProduct(id, fieldToUpdate, newValue) {
+  async deleteProducts(id) {
     try {
-      const product = this.products.find((prod) => prod.id === id);
-      if (!product) {
-        console.log("El producto no fue encontrado");
-      }
-
-      product[fieldToUpdate] = newValue;
-
-      fs.writeFile(
-        this.path,
-        JSON.stringify(this.products, null, 2),
-        (error) => {
-          if (error) {
-            console.log("Error al guardar los cambios en el PRODUCTS");
-          } else {
-            console.log("El producto fue actualizado correctamente");
-          }
-        }
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  // Elimina un producto por su ID
-  async deleteProduct(id) {
-    try {
-      const index = this.products.findIndex((prod) => prod.id === id);
-
-      if (index === -1) {
-        console.log(`No se encontro ningún producto con el ID numero: ${id}`);
-        return;
-      } else {
-        console.log("El elemento del PRODUCTS se eliminó correctamente");
-      }
-      // Elimina el producto de la lista
-      this.products.splice(index, 1);
-
-      fs.readFile(PRODUCTS, "utf-8", (error, data) => {
-        if (error) {
-          console.log("Ocurrió un error al leer el PRODUCTS");
-        }
-
-        let productsData = JSON.parse(data);
-
-        productsData = this.products;
-        const contenidoActualizado = JSON.stringify(productsData, null, 2);
-
-        fs.writeFile(PRODUCTS, contenidoActualizado, (error) => {
-          if (error) {
-            console.log("Hubo un error al actualizar  PRODUCTS");
-          }
-        });
-      });
+      this.products = this.products.filter(product => product.id !== id);
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
     } catch (error) {
       console.log(error);
     }
   }
 }
 
-(async () => {
+const product1 = {
+  title: 'Product 1',
+  description: 'este es el producto 1',
+  price: 10,
+  thumbnail: 'thumbnail-url',
+  code: 'abc1',
+  stock: 10
+}
+
+const product2 = {
+  title: 'Product 2',
+  description: 'este es el producto 2',
+  price: 10,
+  thumbnail: 'ABC123',
+  code: 'abc21',
+  stock: 2
+}
+
+const product3 = {
+  title: 'Product 3',
+  description: 'este es el producto 3',
+  price: 2,
+  thumbnail: 'thumbnail-url',
+  code: '5671235',
+  stock: 7,
+} 
+
+
+const products = async () => {
   try {
-    let test = new ProductManager();
-    console.log(test.getProducts());
-    await test.addProduct(
-      "Producto prueba",
-      "Este es un producto prueba",
-      200,
-      "Sin imagen",
-      "abc123",
-      25
-    );
-    await test.addProduct(
-      "Producto prueba 2",
-      "Este es un producto prueba 2",
-      200,
-      "Sin imagen",
-      "abc321",
-      25
-    );
-    await test.addProduct(
-      "Producto prueba 3",
-      "Este es un producto prueba 3",
-      200,
-      "Sin imagen",
-      "cba123",
-      25
-    );
-    console.log(test.getProducts());
-    await test.getProductById(2);
-    await test.getProductById(5);
-    await test.updateProduct(2, "price", 500);
-    await test.deleteProduct(3);
-    await test.deleteProduct(5);
+    const productManager = new ProductManager()
+    await productManager.addProduct(product1);
+    await productManager.addProduct(product2);
+    await productManager.addProduct(product3);
+    // const getProduct = productManager.getProductById(2);
+    const getProducts = await productManager.getProducts();
+    const updateProduct = await productManager.updateProduct(3, 'title', 'Product 3 actualizado');
+    const deleteProduct = await productManager.deleteProducts(5);
+
+    console.log(getProducts);
   } catch (error) {
     console.log(error);
   }
-})();
+}
+
+products()
